fff5b1c7f244d1b0128dd4d4a96c1033a153cf56
Navya ganait <103976227+navyaganait@users.noreply.github.com>
2023-10-19 22:31:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let typed=new Typed(".auto-input",{
+	strings:['Student','Aspiring Software Engineer','Learner'],
+	typeSpeed:100,
+	backSpeed:100,
+	backDelay:2000,
+	loop: true,
+	// smartBackspace:true,
+	// cursor:next-line,  
+	showCursor: false,
+})
+console.log(typed.js)
+
